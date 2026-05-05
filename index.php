ddaf1ad8760556b452e37f3cd1ac2d0507f8c6ad
Dimasanetu84 <k869ye178@gmail.com>
2026-05-05 15:20:53 +0300

    Main_page

---
+++ index.php
@@ -0,0 +1,10 @@
+<!DOCTYPE html>
+<html lang="ru">
+<head>
+    <meta charset="UTF-8">
+    <title>Hello World</title>
+</head>
+<body>
+    <h3>Проверка загрузки сайта из удалённого репозитория на GitHub</h3>
+</body>
+</html>
